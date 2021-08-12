b24b282288faf9cb71fe09a4949a5d13df2b09ce
steph <sappho@sappho.io>
2021-08-12 03:11:35 -0400

    add nondocker support

--- ipt.sh
+++ ipt.sh
@@ -6,12 +6,30 @@
 # with influence from https://forums.alliedmods.net/showthread.php?t=151551
 # by sappho.io
 
+usedocker=false
+
+# a guess
+if ps aux | grep docker-proxy | grep 27015 &> /dev/null; then
+    usedocker=true
+fi
+
+
+# this isnt just for fun, this lets me easily grep for the rules and delete and recreate them
 comment="-m comment --comment="sappho.io""
 logprefix="[srcds ipt]"
 portmin=27000
-portmax=30000
-conntrack="-m conntrack --ctdir ORIGINAL --ctorigdstport ${portmin}:${portmax} "
-dockeruser="DOCKER-USER"
+portmax=28015
+ports=""
+chain=""
+
+if [[ ${usedocker} == true ]]; then
+    ports="-m conntrack --ctdir ORIGINAL --ctorigdstport ${portmin}:${portmax} "
+    chain="DOCKER-USER"
+    echo "Detected docker."
+else
+    ports="-m multiport --dports ${portmin}:${portmax} "
+    chain="INPUT"
+fi
 
 ## Delete any existing rules we already wrote
 ##
@@ -27,9 +45,9 @@ iptables-restore < /tmp/ipt_scrub
 ##
 ## There should never be any UDP packets below 32 bytes.
 ## drop em
-iptables -I ${dockeruser} 1 -p udp ${comment} ${conntrack} -m length --length 0:32 -j DROP
+iptables -I ${chain} 1 -p udp ${comment} ${ports} -m length --length 0:32 -j DROP
 ## log em
-iptables -I ${dockeruser} 1 -p udp ${comment} ${conntrack} -m length --length 0:32 -j LOG \
+iptables -I ${chain} 1 -p udp ${comment} ${ports} -m length --length 0:32 -j LOG \
 --log-ip-options --log-prefix "${logprefix} XtraSmallJunk "
 
 ## PACKETS THAT ARE TOO BIG
@@ -38,9 +56,9 @@ iptables -I ${dockeruser} 1 -p udp ${comment} ${conntrack} -m length --length 0:
 ##                                          1260             +  1              *  1260
 ## = 2521
 ## drop em
-iptables -I ${dockeruser} 1 -p udp ${comment} ${conntrack} -m length --length 2521:65535 -j DROP
+iptables -I ${chain} 1 -p udp ${comment} ${ports} -m length --length 2521:65535 -j DROP
 ## log em
-iptables -I ${dockeruser} 1 -p udp ${comment} ${conntrack} -m length --length 2521:65535 -j LOG \
+iptables -I ${chain} 1 -p udp ${comment} ${ports} -m length --length 2521:65535 -j LOG \
 --log-ip-options --log-prefix "${logprefix} XtraLargeJunk "
 
 
@@ -49,10 +67,10 @@ iptables -I ${dockeruser} 1 -p udp ${comment} ${conntrack} -m length --length 25
 ## We should never be seeing 1k packets a second from the same ip lol
 ##
 ## drop em
-iptables -I ${dockeruser} 1 -p udp ${comment} ${conntrack} \
+iptables -I ${chain} 1 -p udp ${comment} ${ports} \
 -m hashlimit --hashlimit-name 1kflood --hashlimit-mode srcip,dstport --hashlimit-above 1000/sec -j DROP
 ## log em
-iptables -I ${dockeruser} 1 -p udp ${comment} ${conntrack} \
+iptables -I ${chain} 1 -p udp ${comment} ${ports} \
 -m hashlimit --hashlimit-name 1kflood --hashlimit-mode srcip,dstport --hashlimit-above 1000/sec -j LOG \
 --log-ip-options --log-prefix "${logprefix} 1k pps "
 
@@ -60,11 +78,11 @@ iptables -I ${dockeruser} 1 -p udp ${comment} ${conntrack} \
 ## Prevent "new" state spam aka a2s spam
 ##
 ## drop em
-iptables -I ${dockeruser} 1 -p udp ${comment} ${conntrack} \
+iptables -I ${chain} 1 -p udp ${comment} ${ports} \
 -m state --state NEW \
 -m hashlimit --hashlimit-name newflood --hashlimit-mode srcip --hashlimit-above 2/s --hashlimit-burst 3 -j DROP
 ## log em
-iptables -I ${dockeruser} 1 -p udp ${comment} ${conntrack} \
+iptables -I ${chain} 1 -p udp ${comment} ${ports} \
 -m state --state NEW \
 -m hashlimit --hashlimit-name newflood --hashlimit-mode srcip --hashlimit-above 2/s --hashlimit-burst 3 -j LOG \
 --log-ip-options --log-prefix "${logprefix} NewStateSpam "
@@ -73,10 +91,16 @@ iptables -I ${dockeruser} 1 -p udp ${comment} ${conntrack} \
 ## Allow "Established" packets so that we dont stomp on legit gamers
 ##
 ##
-iptables -I ${dockeruser} 1 -p udp ${comment} -m state --state ESTABLISH -j ACCEPT
+iptables -I ${chain} 1 -p udp ${comment} -m state --state ESTABLISH -j ACCEPT
 
 
 ## Persist them - needs iptables-persistant
 iptables-save > /etc/iptables/rules.v4
 
-echo "Hardened SRCDS".
+cat /etc/iptables/rules.v4 | grep sapph
+
+if [[ ${usedocker} == true ]]; then
+    echo "Hardened SRCDS (in docker)."
+else
+    echo "Hardened SRCDS."
+fi
