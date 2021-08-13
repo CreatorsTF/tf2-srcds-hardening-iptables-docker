9d7d4013c6a390c64540356383ea067a66a2774f
sapphonie <sappho@sappho.io>
2021-08-12 20:09:21 -0400

    Update ipt.sh

--- ipt.sh
+++ ipt.sh
@@ -5,7 +5,9 @@
 # with influence from https://forums.alliedmods.net/showthread.php?t=151551
 # by sappho.io
 
+
 usedocker=false
+
 
 # a guess
 if ps aux | grep docker-proxy | grep 27015 &> /dev/null; then
@@ -16,12 +18,13 @@ fi
 # this isnt just for fun, this lets me easily grep for the rules and delete and recreate them
 comment="-m comment --comment="sappho.io""
 logprefix="[srcds ipt]"
-# log up to every 10 seconds at max so we dont hog io
-loglimit="-m limit --limit 6/min"
+# log up to every 30 seconds at max so we dont hog io
+loglimit="-m limit --limit 2/min"
 portmin=27000
 portmax=28015
 ports=""
 chain=""
+
 
 if [[ ${usedocker} == true ]]; then
     ports="-m conntrack --ctdir ORIGINAL --ctorigdstport ${portmin}:${portmax} "
@@ -32,7 +35,10 @@ else
     chain="INPUT"
 fi
 
+
 echo ""
+
+
 ## Delete any existing rules we already wrote
 ##
 ##
@@ -66,18 +72,19 @@ ${loglimit} --log-ip-options --log-prefix "${logprefix} < XtraLargeJunk > "
 
 ## Prevent UDP spam
 ##
-## We should never be seeing 500 packets a second from the same ip lol
+## We should never be seeing 300 packets a second from the same ip lol
 ##
 ## drop em
 iptables -I ${chain} 1 -p udp ${comment} ${ports} \
--m hashlimit --hashlimit-name 500flood --hashlimit-mode srcip --hashlimit-above 500/sec -j DROP
+-m hashlimit --hashlimit-name 500flood --hashlimit-mode srcip --hashlimit-above 300/sec -j DROP
 # log em
 iptables -I ${chain} 1 -p udp ${comment} ${ports} \
--m hashlimit --hashlimit-name 500flood --hashlimit-mode srcip --hashlimit-above 500/sec -j LOG \
+-m hashlimit --hashlimit-name 500flood --hashlimit-mode srcip --hashlimit-above 300/sec -j LOG \
 ${loglimit} --log-ip-options --log-prefix "${logprefix} < 500 pps > "
 
-## Prevent port flooding
-##
+## Prevent flooding, typically caused by A2S spam
+## Used to check packetstate = NEW, but there's no reason to as we already allow legit packets with our later ALLOW ESTABLISHED rule
+## Thank you arie for the recommendation
 ## drop em
 iptables -I ${chain} 1 -p udp ${comment} ${ports} \
 -m hashlimit --hashlimit-name a2sflood --hashlimit-mode srcip --hashlimit-above 1/s --hashlimit-burst 3 -j DROP
@@ -87,15 +94,18 @@ iptables -I ${chain} 1 -p udp ${comment} ${ports} \
 ${loglimit} --log-ip-options --log-prefix "${logprefix} < A2s Spam > "
 
 ## Allow "Established" packets so that we dont stomp on legit gamers
-##
+## This rule goes last so it gets inserted first
 ##
 iptables -I ${chain} 1 -p udp ${comment} -m state --state ESTABLISH -j ACCEPT
 
 
-## Persist them - needs iptables-persistant
+## Persist them - needs iptables-persistant!
 iptables-save > /etc/iptables/rules.v4
 
+
+## Dump our generated rules
 cat /etc/iptables/rules.v4 | grep sapph
+
 
 echo ""
 if [[ ${usedocker} == true ]]; then
