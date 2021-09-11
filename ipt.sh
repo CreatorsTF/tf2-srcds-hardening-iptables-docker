e15181687790ca2405935b3994cf126adc200fd0
sapphonie <sappho@sappho.io>
2021-09-11 11:28:38 -0400

    Rework to use prerouting raw

--- ipt.sh
+++ ipt.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+	#!/bin/bash
 # sourceds hardening with iptables and docker
 # this *should* hopefully prevent most petty/smallish a2s attacks
 # VALVE I *SHOULD NOT* HAVE HAD TO WRITE THIS
@@ -11,15 +11,13 @@
 # CONFIG
 #########################################################################
 
-# for crontabbing
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # this isnt just for fun, this lets me easily grep for the rules and delete and recreate them
 # i know this commenting is weird, bash sucks ass
 COMMENT="-m comment --comment="srcds-hardening-by-sappho.io""
-LOGPREFIX="[srcds-ipt]"
+LOGPREFIX="<|srcds-ipt|>"
 # log up to every 60 seconds at max so we dont hog io
-LOGLIMIT="-m limit --limit 1/min"
+LOGLIMIT="-m limit --limit 10000/min"
 
 LOGLIMIT_FAST="-m limit --limit 100/min"
 
@@ -74,7 +72,7 @@ iptables-restore -c < /tmp/ipt_scrub
 
 
 # create our ipset rules - requires ipset!
-ipset create permatrusted hash:ip    timeout 0     || true
+ipset create permatrusted hash:ip      timeout 0   || true
 ipset create  signed_on   hash:ip,port timeout 240 || true
 
 
@@ -83,7 +81,7 @@ ipset create  signed_on   hash:ip,port timeout 240 || true
 #########################################################################
 
 
-# 7: PACKET TOO SMOL
+# 12: PACKET TOO SMOL
 # There should never be any packets packets below 32 bytes
 RULE_FILTER="-m length --length 0:32"
 
@@ -95,7 +93,7 @@ ${ipt_pre_raw} -p udp -i ${defaultin} ${COMMENT} ${ports} ${RULE_FILTER}    \
     --log-prefix "${LOGPREFIX} len < 32: "
 
 
-# 6: PACKET TOO BIG
+##### 11: PACKET TOO BIG
 # There should never be any packets above this length:
 # (net_maxroutable) + (net_splitrate  * net_maxfragments)
 #  1260             + (1              *  1260)
@@ -111,50 +109,81 @@ ${ipt_pre_raw} -p udp -i ${defaultin} ${COMMENT} ${ports} ${RULE_FILTER}    \
 
 RULE_FILTER="-m string --algo bm --hex-string"
 
-# 5: Log client signons - string is at the end of the packet
-${ipt_pre_raw} -p udp -i ${defaultin} ${COMMENT}                            \
+##### 10: Log client signons - string is at the end of the packet
+${ipt_pre_raw} -p udp -i ${defaultin} ${COMMENT} ${ports}                   \
     ${RULE_FILTER} '|3030303030303030303000|'                               \
-    -m length --length 48 --from 26 --to 48                                  \
+    -m length --length 48 --from 26 --to 48                                 \
     -j LOG ${LOGLIMIT_FAST} --log-ip-options --log-level error              \
     --log-prefix "${LOGPREFIX} signon: "
 
-# 4: Grab client signon packets to whitelist
-${ipt_pre_raw} -p udp -i ${defaultin} ${COMMENT}                            \
+##### 9: Grab client signon packets to whitelist
+${ipt_pre_raw} -p udp -i ${defaultin} ${COMMENT} ${ports}                   \
     ${RULE_FILTER} '|3030303030303030303000|'                               \
-    -m length --length 48 --from 26 --to 48                                  \
+    -m length --length 48 --from 26 --to 48                                 \
     -j SET --add-set signed_on src,dst --timeout 120
 
-# 3: Log client signoffs - string is at the end of the packet
-${ipt_pre_raw} -p udp -i ${defaultin} ${COMMENT}                            \
+##### 8: Log client signoffs - string is at the end of the packet
+${ipt_pre_raw} -p udp -i ${defaultin} ${COMMENT} ${ports}                   \
     ${RULE_FILTER} '|9b5bd9181d88581e48dd5c999c0bc0|'                       \
     -m length --length 65 --from 35 --to 65                                 \
     -j LOG ${LOGLIMIT_FAST} --log-ip-options --log-level error              \
     --log-prefix "${LOGPREFIX} signoff: "
 
-# 2: Grab client signoff packets to unwhitelist clients
+##### 7: Grab client signoff packets to unwhitelist clients
 # They will timeout after ~65 seconds if no data is received anyway.
-${ipt_pre_raw} -p udp -i ${defaultin} ${COMMENT}                            \
+${ipt_pre_raw} -p udp -i ${defaultin} ${COMMENT} ${ports}                   \
     ${RULE_FILTER} '|9b5bd9181d88581e48dd5c999c0bc0|'                       \
     -m length --length 65 --from 35 --to 65                                 \
     -j SET --del-set signed_on src,dst
 
+########## Ignore signed on and trusted users for these next two rules
+RULE_FILTER="-m hashlimit --hashlimit-name speedlimit --hashlimit-mode srcip,dstport --hashlimit-above 3/sec --hashlimit-burst 5"
 
+NOMATCH_TRUSTED=" -m set ! --match-set permatrusted  src    "
+NOMATCH_SIGNEDON="-m set ! --match-set signed_on     src,dst"
 
-# 1: ALWAYS allow trusted hosts
+##### 6: UDP Spam
+# Should never see so much traffic from the same IP to the same port
+${ipt_pre_raw} -p udp -i ${defaultin} ${COMMENT} ${ports} ${RULE_FILTER}    \
+    ${NOMATCH_TRUSTED} ${NOMATCH_SIGNEDON}                                  \
+    -j DROP
+
+##### 5: Log udp spam
+${ipt_pre_raw} -p udp -i ${defaultin} ${COMMENT} ${ports} ${RULE_FILTER}    \
+    ${NOMATCH_TRUSTED} ${NOMATCH_SIGNEDON}                                  \
+    -j LOG ${LOGLIMIT} --log-ip-options                                     \
+    --log-prefix "${LOGPREFIX} udp spam: "
+
+##### 4: Allow signed on gamers to play - only whitelists ip to destip / dport :D
+${ipt_pre_raw} -p udp -i ${defaultin} ${COMMENT}                            \
+    -m set --match-set signed_on src,dst                                    \
+    -j ACCEPT
+
+##### 3: Reset timeout whenever we get some data from this client
+${ipt_pre_raw} -p udp ${COMMENT}                                            \
+    -m set --match-set signed_on src,dst                                    \
+    -j SET --add-set   signed_on src,dst --timeout 65 --exist
+
+##### 3: Debug logging
+#${ipt} -p udp ${COMMENT}                                                    \
+#    -m set --match-set signed_on src,dst                                    \
+#    -j LOG ${LOGLIMIT_FAST} --log-ip-options                                \
+#    --log-prefix "${LOGPREFIX} whitelist: "
+
+##### 2: ALWAYS allow trusted hosts
 # Uses /etc/hosts.trusted to use a list of hosts to allow unrestricted communication.
-    if [[ -f /etc/hosts.trusted ]]; then
-        for host in $(cat /etc/hosts.trusted); do
+trustedhosts="/etc/hosts.trusted"
+if [[ -f "$trustedhosts" ]]; then
+    while IFS= read -r line
+    do
+        #${ipt_pre_raw} -p udp ${COMMENT} -s "$line"                         \
+        #    -j SET --add-set permatrusted src
+        echo "$line added to trusted hosts"
+    done < "$trustedhosts"
+fi
 
-${ipt_pre_raw} -p udp ${COMMENT} -s "$host"                                 \
-    -j SET --add-set permatrusted src --exist
 
-        done
-        echo "allowing trusted hosts"
-    fi
-
-
-# 0: Disable conntracking on our srcds ports
-
+##### 1: Disable conntracking on our srcds ports
 if [[ ${usedocker} == true ]]; then
     # if we're using docker we need to disable conntrack wholesale
     # docker needs the whole thing gone, else it will get very confused and not route things properly
@@ -167,14 +196,11 @@ else
         -j NOTRACK
 fi
 
-
-
 #########################################################################
 # PREROUTING MANGLE
 #########################################################################
 
-
-# 1: Drop invalid packets
+##### 1: Drop invalid packets
 RULE_FILTER="-m state --state INVALID"
 
 ${ipt_pre_mangle} -p all ${COMMENT} ${RULE_FILTER}                          \
@@ -182,50 +208,7 @@ ${ipt_pre_mangle} -p all ${COMMENT} ${RULE_FILTER}                          \
 
 ${ipt_pre_mangle} -p all ${COMMENT} ${RULE_FILTER}                          \
     -j LOG ${LOGLIMIT} --log-ip-options                                     \
-    --log-prefix "${LOGPREFIX} INVALID PACKET: "
-
-
-#########################################################################
-# INPUT / DOCKER-USER
-#########################################################################
-
-
-RULE_FILTER="-m hashlimit --hashlimit-name speedlimit --hashlimit-mode srcip,dstport --hashlimit-above 3/sec --hashlimit-burst 10"
-# Ignore signed on and trusted users
-NOMATCH_TRUSTED=" -m set ! --match-set permatrusted  src    "
-NOMATCH_SIGNEDON="-m set ! --match-set signed_on     src,dst"
-
-# 4: UDP Spam
-# Should never see so much traffic from the same IP to the same port
-${ipt} -p udp -i ${defaultin} ${COMMENT} ${ports} ${RULE_FILTER}            \
-    ${NOMATCH_TRUSTED} ${NOMATCH_SIGNEDON}                                  \
-    -j DROP
-
-# 3: Log udp spam
-${ipt} -p udp -i ${defaultin} ${COMMENT} ${ports} ${RULE_FILTER}            \
-    ${NOMATCH_TRUSTED} ${NOMATCH_SIGNEDON}                                  \
-    -j LOG ${LOGLIMIT} --log-ip-options                                     \
-    --log-prefix "${LOGPREFIX} udpspam: "
-
-
-# 2: Allow signed on gamers to play - only whitelists ip to destip / dport :D
-${ipt} -p udp -i ${defaultin} ${COMMENT}                                    \
-    -m set --match-set signed_on src,dst                                    \
-    -j ACCEPT
-
-# 1: Reset timeout whenever we get some data from this client
-${ipt} -p udp ${COMMENT}                                                    \
-    -m set --match-set signed_on src,dst                                    \
-    -j SET --add-set   signed_on src,dst --timeout 65 --exist
-
-
-# 0: Debug logging
-#${ipt} -p udp ${COMMENT}                                                    \
-#    -m set --match-set signed_on src,dst                                    \
-#    -j LOG ${LOGLIMIT_FAST} --log-ip-options                                \
-#    --log-prefix "${LOGPREFIX} whitelist: "
-
-
+    --log-prefix "${LOGPREFIX} INVALID PKT: "
 
 #########################################################################
 # CLEANUP
@@ -246,3 +229,4 @@ if [[ ${usedocker} == true ]]; then
 else
     echo "Hardened SRCDS."
 fi
+
